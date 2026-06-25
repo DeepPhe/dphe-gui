0715385ed9fd431274d9a19b1cef14bf4f8afbe0
Sean Finan <sean.finan@childrens.harvard.edu>
2026-06-25 13:51:34 -0400

    Icon changes, project table code brevity

--- src/main/java/org/healthnlp/deepphe/gui/DesktopMainPanel.java
+++ src/main/java/org/healthnlp/deepphe/gui/DesktopMainPanel.java
@@ -16,8 +16,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
+import static org.healthnlp.deepphe.gui.DesktopMainPanel.ButtonInfo.*;
 
 /**
  * @author SPF , chip-nlp
@@ -27,25 +29,46 @@ public class DesktopMainPanel extends JPanel {
 
     static private final Logger LOGGER = Logger.getLogger( "DeepPhe Desktop" );
 
-    static private final String DPHE_NAME = "Patient Phenotype Summarizer";
-    static private final String ETL_NAME = "Database Importer";
-    static private final String VIZ_NAME = "DeepPhe Visualization Tool";
-    static private final String WIKI_NAME = "DeepPhe Wiki (help)";
-    static private final String HELP_NAME = "DeepPhe Web Site";
+    enum ButtonInfo {
+        DPHE( "NLP Summarizer", "NLP_3_100.png" ),
+        ETL( "Data Merge Tool", "ETL_3_100.png" ),
+        VIZ( "Visualization GUI", "Viz_4_100.png" ),
+        WIKI( "Wiki", "Wiki_1_80.png" ),
+        SITE( "Web Site", "Website_1_80.png" );
+        private final String _name;
+        private final String _icon;
+        ButtonInfo( final String name, final String icon ) {
+            _name = name;
+            _icon = icon;
+        }
+        private String getName() {
+            return _name;
+        }
+        private String getIcon() {
+            return "org/healthnlp/deepphe/desktop/icon/" + _icon;
+        }
+    }
+
+
+    private static final String HTTPS_DEEPPHE_NLP_WIKI = "https://deepphe.github.io/";
     private static final String HTTPS_DEEPPHE_GITHUB_IO = "https://deepphe.github.io/";
 
     private final Map<String,String> _parameterMap = new HashMap<>();
 
+    private final ProjectPanel _projectPanel;
     private JButton _dpheButton;
     private JButton _etlButton;
     private JButton _vizButton;
-    private JButton _helpButton;
+    private JButton _wikiButton;
+    private JButton _siteButton;
+
 
 
     DesktopMainPanel() {
         super( new BorderLayout() );
         final JPanel subPanel = new JPanel( new BorderLayout() );
-        subPanel.add( createProjectPanel(), BorderLayout.NORTH );
+        _projectPanel = createProjectPanel();
+        subPanel.add( _projectPanel, BorderLayout.NORTH );
         subPanel.add( createToolBar(), BorderLayout.CENTER );
         add( subPanel, BorderLayout.NORTH );
         add( createLogPanel(), BorderLayout.CENTER );
@@ -59,7 +82,7 @@ public class DesktopMainPanel extends JPanel {
         }
        String parameterFile = args[ 0 ];
         if ( !ParameterHandler.readMapFromFile( parameterFile, _parameterMap ) ) {
-           LOGGER.error( "Cannot read the parameter file: " + parameterFile );
+            LOGGER.error( "Cannot read the parameter file: " + parameterFile );
             LOGGER.error( "Please exit the application and correct your parameter file." );
             return;
         }
@@ -84,26 +107,27 @@ public class DesktopMainPanel extends JPanel {
         if ( !ParameterHandler.isValueValid( dpheDir ) ) {
             return;
         }
-        _dpheButton.addActionListener( new StartAction( DPHE_NAME, startDphe, dpheDir) );
+        _dpheButton.addActionListener( new StartAction( DPHE, startDphe, dpheDir, _projectPanel::getPiperGuiParms ) );
 //        TODO - ETL Action
-//        final String startEtl
-//              = ParameterHandler.getAndCheckParameter( _parameterMap, parameterFile, "StartEtl", "StartDbLoad", "StartLoadDb" );
-//        if ( !ParameterHandler.isValueValid( startEtl ) ) {
-//            return;
-//        }
-//        final String etlDir
-//              = ParameterHandler.getAndCheckParameter( _parameterMap, parameterFile, "EtlDir", "DbLoadDir", "LoadDbDir" );
-//        if ( !ParameterHandler.isValueValid( etlDir ) ) {
-//            return;
-//        }
-//        _etlButton.addActionListener( new StartAction( ETL_NAME, startEtl, etlDir) );
+        final String startEtl
+              = ParameterHandler.getAndCheckParameter( _parameterMap, parameterFile, "StartEtl", "StartDbLoad", "StartLoadDb" );
+        if ( !ParameterHandler.isValueValid( startEtl ) ) {
+            return;
+        }
+        final String etlDir
+              = ParameterHandler.getAndCheckParameter( _parameterMap, parameterFile, "EtlDir", "DbLoadDir", "LoadDbDir" );
+        if ( !ParameterHandler.isValueValid( etlDir ) ) {
+            return;
+        }
+        _etlButton.addActionListener( new StartAction( ETL, startEtl, etlDir, _projectPanel::getEtlParms ) );
         final String startViz
               = ParameterHandler.getAndCheckParameter( _parameterMap, parameterFile, "StartViz", "StartVis" );
         if ( !ParameterHandler.isValueValid( startViz ) ) {
             return;
         }
-        _vizButton.addActionListener( new StartAction( VIZ_NAME, startViz, vizDir) );
-        _helpButton.addActionListener( new HelpAction() );
+        _vizButton.addActionListener( new StartAction( VIZ, startViz, vizDir, _projectPanel::getVizParms ) );
+        _wikiButton.addActionListener( new WebAction( HTTPS_DEEPPHE_NLP_WIKI ) );
+        _siteButton.addActionListener( new WebAction( HTTPS_DEEPPHE_GITHUB_IO ) );
     }
 
 
@@ -129,7 +153,7 @@ public class DesktopMainPanel extends JPanel {
     }
 
 
-    private JComponent createProjectPanel() {
+    private ProjectPanel createProjectPanel() {
         return new ProjectPanel();
     }
 
@@ -139,27 +163,30 @@ public class DesktopMainPanel extends JPanel {
         toolBar.setFloatable( false );
         toolBar.setRollover( true );
         toolBar.addSeparator( new Dimension( 50, 0 ) );
-        _dpheButton = addButton( toolBar, DPHE_NAME );
-        toolBar.addSeparator( new Dimension( 100, 0 ) );
-        _etlButton = addButton( toolBar, ETL_NAME );
-        toolBar.addSeparator( new Dimension( 100, 0 ) );
-        _vizButton = addButton( toolBar, VIZ_NAME );
-        toolBar.addSeparator( new Dimension( 100, 0 ) );
-        _helpButton = addButton( toolBar, HELP_NAME );
+        _dpheButton = addButton( toolBar, DPHE );
+        toolBar.addSeparator( new Dimension( 50, 0 ) );
+        _etlButton = addButton( toolBar, ETL );
+        toolBar.addSeparator( new Dimension( 50, 0 ) );
+        _vizButton = addButton( toolBar, VIZ );
+        toolBar.addSeparator( new Dimension( 50, 0 ) );
+        toolBar.add( new JSeparator( SwingConstants.VERTICAL ) );
+        _wikiButton = addButton( toolBar, WIKI );
+        toolBar.addSeparator( new Dimension( 10, 0 ) );
+        _siteButton = addButton( toolBar, SITE );
         toolBar.addSeparator( new Dimension( 50, 0 ) );
         return toolBar;
     }
 
-    static private JButton addButton( final JToolBar toolBar, final String toolTip ) {
+    static private JButton addButton( final JToolBar toolBar, final ButtonInfo buttonInfo ) {
         final JButton button = new JButton();
         button.setFocusPainted( false );
         // prevents first button from having a painted border
         button.setFocusable( false );
-        button.setToolTipText( toolTip );
+        button.setToolTipText( buttonInfo.getName() );
         button.setHorizontalTextPosition( SwingConstants.CENTER );
         button.setVerticalTextPosition( SwingConstants.BOTTOM );
-        button.setFont( new Font(Font.SERIF, Font.BOLD, 18 ) );
-        button.setText( toolTip );
+        button.setFont( new Font(Font.SANS_SERIF, Font.BOLD, 16 ) );
+        button.setText( buttonInfo.getName() );
         toolBar.add( button );
         toolBar.addSeparator( new Dimension( 10, 0 ) );
         return button;
@@ -170,12 +197,14 @@ public class DesktopMainPanel extends JPanel {
         private final String _name;
         private final String _command;
         private final String _dir;
+        private final Supplier<String> _parmFx;
         private boolean _paused = false;
 
-        private StartAction( final String name, final String command, final String dir ) {
-            _name = name;
+        private StartAction( final ButtonInfo buttonInfo, final String command, final String dir, final Supplier<String> parmFx ) {
+            _name = buttonInfo.getName();
             _command = command;
             _dir = dir;
+            _parmFx = parmFx;
         }
 
         @Override
@@ -184,7 +213,7 @@ public class DesktopMainPanel extends JPanel {
                 return;
             }
             _paused = true;
-            final SystemUtil.CommandRunner runner = new SystemUtil.CommandRunner( _command );
+            final SystemUtil.CommandRunner runner = new SystemUtil.CommandRunner( _command + " " + _parmFx.get() );
             runner.setLogger( LOGGER );
             if ( _dir != null && !_dir.isEmpty() ) {
                 runner.setDirectory( _dir );
@@ -201,13 +230,18 @@ public class DesktopMainPanel extends JPanel {
         }
     }
 
-    private final class HelpAction implements ActionListener {
+    private static final class WebAction implements ActionListener {
+        private final String _site;
+        private WebAction( final String site ) {
+            _site = site;
+        }
         @Override
         public void actionPerformed( final ActionEvent event ) {
-            if ( _helpButton == null ) {
-                return;
-            }
-            SystemUtil.openWebPage( HTTPS_DEEPPHE_GITHUB_IO );
+//            if ( _siteButton == null ) {
+//                return;
+//            }
+            LOGGER.info( "Opening " + _site + " ..." );
+            SystemUtil.openWebPage( _site );
         }
     }
 
@@ -219,16 +253,16 @@ public class DesktopMainPanel extends JPanel {
     private final class ButtonIconLoader implements Runnable {
         @Override
         public void run() {
-            final String dir = "org/healthnlp/deepphe/desktop/icon/";
-            final String dphePng = "StartPFS_144_4.png";
-            final String vizPng = "StartViz_144_3.png";
-            final String helpPng = "Info_144_3.png";
-            final Icon dpheIcon = IconLoader.loadIcon( dir + dphePng );
-            final Icon vizIcon = IconLoader.loadIcon( dir + vizPng );
-            final Icon helpIcon = IconLoader.loadIcon( dir + helpPng );
+            final Icon dpheIcon = IconLoader.loadIcon( DPHE.getIcon() );
+            final Icon etlIcon = IconLoader.loadIcon( ETL.getIcon() );
+            final Icon vizIcon = IconLoader.loadIcon( VIZ.getIcon() );
+            final Icon nlpWikiIcon = IconLoader.loadIcon( WIKI.getIcon() );
+            final Icon siteIcon = IconLoader.loadIcon( SITE.getIcon() );
             _dpheButton.setIcon( dpheIcon );
+            _etlButton.setIcon( etlIcon );
             _vizButton.setIcon( vizIcon );
-            _helpButton.setIcon( helpIcon );
+            _wikiButton.setIcon( nlpWikiIcon );
+            _siteButton.setIcon( siteIcon );
         }
     }
 
--- src/main/java/org/healthnlp/deepphe/gui/ProjectPanel.java
+++ src/main/java/org/healthnlp/deepphe/gui/ProjectPanel.java
@@ -1,7 +1,6 @@
 package org.healthnlp.deepphe.gui;
 
 import org.apache.ctakes.gui.component.FileTableCellEditor;
-import org.apache.ctakes.gui.component.SmoothTipTable;
 import org.apache.log4j.Logger;
 import org.healthnlp.deepphe.util.ParameterHandler;
 
@@ -11,13 +10,24 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.event.EventListenerList;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableModel;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.io.*;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import static javax.swing.JFileChooser.FILES_ONLY;
+import static javax.swing.JFileChooser.DIRECTORIES_ONLY;
+
 
 /**
  * @author SPF , chip-nlp
@@ -31,22 +41,20 @@ public class ProjectPanel extends JPanel {
 
    static private final String DEFAULT_PIPER_FILE = CWD + "/resources/pipeline/DeepPheDefault.piper";
    static private final String DEFAULT_TEXT_CORPUS = CWD + "/resources/examples/example_corpus";
-   static private final String DEFAULT_OMOP_DB = CWD + "/resources/examples/example_omop/fake.db";
+   static private final String DEFAULT_OMOP_DB = CWD + "/resources/examples/example_omop/patient_demographics.json";
    static private final String DEFAULT_OUTPUT_DIR = CWD + "/resources/examples/example_output";
 
    static private final String EXAMPLE_PROJECT = "ExampleProject";
    static private final String PROJECTS_DIR = CWD + "/resources/projects/";
 
    static private final String PROJECT = "PROJECT";
-   static private final String PIPER_FILE = "PIPELINE";
+   static private final String PIPER_FILE = "PIPER_FILE";
    static private final String TEXT_CORPUS = "TEXT_CORPUS";
    static private final String OMOP_DB = "OMOP_DB";
    static private final String OUTPUT_DIR = "OUTPUT_DIR";
 
 
    static private final String PROJECT_NAME = "Project:";
-   static private final String[] ROW_NAMES = { " Pipeline", " Corpus Directory",
-                                               " OMOP Database", " Output Directory" };
 
    static private final String _projectListPath = PROJECTS_DIR + "ProjectList.txt";
    private final ArrayList<String> _projectList = new ArrayList<>();
@@ -77,7 +85,7 @@ public class ProjectPanel extends JPanel {
       registerShutdownHook();
    }
 
-   private String getProjectName() {
+   public String getProjectName() {
       return _projectMap.computeIfAbsent( PROJECT, k -> EXAMPLE_PROJECT );
    }
 
@@ -102,7 +110,7 @@ public class ProjectPanel extends JPanel {
    }
 
 
-   private String getPiperFile() {
+   public String getPiperFile() {
       return _projectMap.computeIfAbsent( PIPER_FILE, k -> DEFAULT_PIPER_FILE );
    }
 
@@ -119,7 +127,7 @@ public class ProjectPanel extends JPanel {
       }
    }
 
-   private String getTextCorpus() {
+   public String getTextCorpus() {
       return _projectMap.computeIfAbsent( TEXT_CORPUS, k -> DEFAULT_TEXT_CORPUS );
    }
 
@@ -132,20 +140,24 @@ public class ProjectPanel extends JPanel {
       }
    }
 
-   private String getOmopDb() {
+   public String getOmopDb() {
       return _projectMap.computeIfAbsent( OMOP_DB, k -> DEFAULT_OMOP_DB );
    }
 
    private void setOmopDb( final String path ) {
-      final File dir = new File( path );
-      if ( dir.isFile() ) {
+      if ( !path.toLowerCase().endsWith( ".json" ) ) {
+         setOmopDb( DEFAULT_OMOP_DB );
+         return;
+      }
+      final File json = new File( path );
+      if ( json.isFile() ) {
          _projectMap.put( OMOP_DB, path );
       } else {
-            setOmopDb( DEFAULT_OMOP_DB );
+         setOmopDb( DEFAULT_OMOP_DB );
       }
    }
 
-   private String getOutputDir() {
+   public String getOutputDir() {
       return _projectMap.computeIfAbsent(OUTPUT_DIR, k -> DEFAULT_OUTPUT_DIR );
    }
 
@@ -159,7 +171,27 @@ public class ProjectPanel extends JPanel {
    }
 
    private JComponent createProjectTable() {
-      final JTable table = new SmoothTipTable( _tableModel );
+//      final JTable table = new SmoothTipTable( _tableModel ) {
+      final JTable table = new JTable( _tableModel ) {
+         @Override
+         public String getToolTipText( final MouseEvent event) {
+            final Point p = event.getPoint();
+            return _tableModel.getToolTip( rowAtPoint( p ), columnAtPoint( p ) );
+         }
+         @Override
+         public TableCellEditor getCellEditor( final int row, final int column ) {
+            if ( column == 2 ) {
+               return _tableModel.getCellEditor( row, column );
+            }
+            return super.getCellEditor( row, column );
+         }
+         public TableCellRenderer getCellRenderer( final int row, final int column ) {
+            if ( column == 2 ) {
+               return _tableModel.getCellEditor( row, column );
+            }
+            return super.getCellRenderer( row, column );
+         }
+      };
       table.setFont( new Font( FONT, Font.PLAIN, 14 ) );
       table.setBorder( new BevelBorder( BevelBorder.LOWERED ) );
       table.putClientProperty( "terminateEditOnFocusLost", true );
@@ -176,15 +208,11 @@ public class ProjectPanel extends JPanel {
                .setMaxWidth( 25 );
       table.setRowSelectionAllowed( true );
       table.setCellSelectionEnabled( true );
-      final FileTableCellEditor fileEditor = new FileTableCellEditor();
-      fileEditor.getFileChooser()
-                .setFileSelectionMode( JFileChooser.FILES_AND_DIRECTORIES );
-      table.setDefaultRenderer( File.class, fileEditor );
-      table.setDefaultEditor( File.class, fileEditor );
       ListSelectionModel selectionModel = table.getSelectionModel();
       selectionModel.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
       return table;
    }
+
 
 
    private class ProjectComboModel extends AbstractListModel<String> implements ComboBoxModel<String> {
@@ -225,21 +253,89 @@ public class ProjectPanel extends JPanel {
 
 
    private final class ProjectTableModel implements TableModel {
-
-      private final String[] COLUMN_NAMES = { "Name", "Value", "" };
-      private final Class<?>[] COLUMN_CLASSES = { String.class, String.class, File.class };
+      private final Consumer<String> setPiperFile1 = ProjectPanel.this::setPiperFile;
+      private final Consumer<String> setTextCorpus1 = ProjectPanel.this::setTextCorpus;
+      private final Consumer<String> setOmopDb1 = ProjectPanel.this::setOmopDb;
+      private final Consumer<String> setOutputDir1 = ProjectPanel.this::setOutputDir;
       private final EventListenerList _listenerList = new EventListenerList();
+      private final FileTableCellEditor _piperChooser = new FileTableCellEditor();
+      private final FileTableCellEditor _corpusChooser = new FileTableCellEditor();
+      private final FileTableCellEditor _jsonChooser = new FileTableCellEditor();
+      private final FileTableCellEditor _outDirChooser = new FileTableCellEditor();
+      private final String[] COLUMN_NAMES = { "Name", "Value", "Explorer" };
+      private final Class<?>[] COLUMN_CLASSES = { String.class, String.class, File.class };
+      private final String[] ROW_NAMES = { " Piper File", " Corpus Directory",
+                                           " OMOP Database", " Output Directory" };
+      private final String[] TOOLTIPS = {  "A Piper File defining a Pipeline.",
+                                           "A directory containing patient directories filled with document files.",
+                                           "A JSON file with demographics in the required OMOP format.",
+                                           "A directory for output from the Phenotype Summarizer and Database Loader." };
+      private final String[] TYPES = { "file", "directory", "file", "directory" };
+      private final Supplier<String>[] GETTERS = new Supplier[]{ ProjectPanel.this::getPiperFile,
+                                                                 ProjectPanel.this::getTextCorpus,
+                                                                 ProjectPanel.this::getOmopDb,
+                                                                 ProjectPanel.this::getOutputDir };
+      private final Consumer<String>[] SETTERS = new Consumer[]{ setPiperFile1,
+                                                         setTextCorpus1,
+                                                         setOmopDb1,
+                                                         setOutputDir1 };
+//      private final FileTableCellEditor[] CHOOSERS = { _piperChooser, _corpusChooser, _jsonChooser, _outDirChooser };
+      private final FileTableCellEditor[] CHOOSERS = { createChooser( FILES_ONLY, "Piper Files", "piper" ),
+                                                       createChooser( DIRECTORIES_ONLY, null, null ),
+                                                       createChooser( FILES_ONLY, "OMOP JSON", "json" ),
+                                                       createChooser( DIRECTORIES_ONLY, null, null ) };
 
+//      private ProjectTableModel() {
+//         _corpusChooser.getFileChooser().setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY );
+//         _outDirChooser.getFileChooser().setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY );
+//         _piperChooser.getFileChooser().setFileSelectionMode( JFileChooser.FILES_ONLY );
+//         _piperChooser.getFileChooser().setFileFilter(
+//               new FileNameExtensionFilter( "Piper Files", "piper" ) );
+//         _jsonChooser.getFileChooser().setFileSelectionMode( JFileChooser.FILES_ONLY );
+//         _jsonChooser.getFileChooser().setFileFilter(
+//               new FileNameExtensionFilter( "OMOP Demographics JSON", "json" ) );
+//      }
+      private FileTableCellEditor createChooser( final int mode, final String filterName, final String filterExt ) {
+         final FileTableCellEditor chooser = new FileTableCellEditor();
+         chooser.getFileChooser().setFileSelectionMode( mode );
+         if ( filterName != null && filterExt != null ) {
+            chooser.getFileChooser().setFileFilter( new FileNameExtensionFilter( filterName, filterExt ) );
+         }
+         return chooser;
+      }
       private void reset() {
          fireTableChanged( new TableModelEvent( this ) );
       }
+      private String getToolTip( final int row, final int column ) {
+         switch ( column ) {
+            case 0 : return TOOLTIPS[ row ];
+            case 1 : return "Type or paste a " + TYPES[ row ] + " path, or drag and drop.";
+            case 2 : return "Click to open a file explorer.";
+         }
+         return "";
+      }
+      private FileTableCellEditor getCellEditor( final int row, final int column ) {
+//         FileTableCellEditor editor;
+//         switch ( row ) {
+//            case 0 : editor = _piperChooser; break;
+//            case 1 : editor = _corpusChooser; break;
+//            case 2 : editor = _jsonChooser; break;
+//            case 3 : editor = _outDirChooser; break;
+//            default: editor = _defaultChooser;
+//         }
+//         editor.getFileChooser().setSelectedFile( (File)getValueAt( row, column ) );
+//         return editor;
+         CHOOSERS[ row ].getFileChooser().setSelectedFile( (File)getValueAt( row, column ) );
+         return CHOOSERS[ row ];
+      }
+
       @Override
       public int getRowCount() {
-         return 4;
+         return ROW_NAMES.length;
       }
       @Override
       public int getColumnCount() {
-         return 3;
+         return COLUMN_NAMES.length;
       }
       @Override
       public String getColumnName( final int column ) {
@@ -254,17 +350,20 @@ public class ProjectPanel extends JPanel {
          if ( column == 0 ) {
             return ROW_NAMES[ row ];
          } else if ( column == 1 ) {
-            switch ( row ) {
-               case 0 : return getPiperFile();
-               case 1 : return getTextCorpus();
-               case 2 : return getOmopDb();
-               case 3 : return getOutputDir();
-            }
+            return normalizePath( GETTERS[ row ].get() );
+//            switch ( row ) {
+//               case 0 : return normalizePath( getPiperFile() );
+//               case 1 : return normalizePath( getTextCorpus() );
+//               case 2 : return normalizePath( getOmopDb() );
+//               case 3 : return normalizePath( getOutputDir() );
+//            }
          } else if ( column == 2 ) {
-            final String path = (String) getValueAt( row, 1 );
-            return new File( path );
+            return new File( (String) getValueAt( row, 1 ) );
          }
          return "ERROR";
+      }
+      private String normalizePath( final String filepath ) {
+         return Paths.get( filepath ).toAbsolutePath().normalize().toString();
       }
       @Override
       public boolean isCellEditable( final int row, final int column ) {
@@ -274,21 +373,23 @@ public class ProjectPanel extends JPanel {
       public void setValueAt( final Object aValue, final int row, final int column ) {
          if ( column == 1 ) {
             final String text = aValue.toString().trim();
-            switch ( row ) {
-               case 0 : setPiperFile( text );
-               case 1 : setTextCorpus( text );
-               case 2 : setOmopDb( text );
-               case 3 : setOutputDir( text );
-            }
+            SETTERS[ row ].accept( text );
+//            switch ( row ) {
+//               case 0 : setPiperFile( text );
+//               case 1 : setTextCorpus( text );
+//               case 2 : setOmopDb( text );
+//               case 3 : setOutputDir( text );
+//            }
             fireTableChanged( new TableModelEvent( this, row, row, column ) );
          } else if ( column == 2 && aValue instanceof File ) {
             final String filePath = ((File)aValue).getPath();
-            switch ( row ) {
-               case 0 : setPiperFile( filePath );
-               case 1 : setTextCorpus( filePath );
-               case 2 : setOmopDb( filePath );
-               case 3 : setOutputDir( filePath );
-            }
+            SETTERS[ row ].accept( filePath );
+//            switch ( row ) {
+//               case 0 : setPiperFile( filePath );
+//               case 1 : setTextCorpus( filePath );
+//               case 2 : setOmopDb( filePath );
+//               case 3 : setOutputDir( filePath );
+//            }
             fireTableChanged( new TableModelEvent( this, row, row, 1 ) );
          }
       }
@@ -358,6 +459,25 @@ public class ProjectPanel extends JPanel {
       } catch ( IOException ioE ) {
          LOGGER.error( "Could not write " + file, ioE );
       }
+   }
+
+   public String getPiperGuiParms() {
+      final File cliFile = new File( PROJECTS_DIR, getProjectName() + ".cli" );
+      try ( Writer writer = new BufferedWriter( new FileWriter( cliFile ) ) ) {
+         writer.write( "InputDirectory=" + getTextCorpus() + "\n" );
+         writer.write( "OutputDirectory=" + getOutputDir() + "\n" );
+      } catch ( IOException ioE ) {
+         LOGGER.error( "Could not write Piper CLI file with project parameters.", ioE );
+      }
+      return "-p " + getPiperFile() + " -c " + cliFile.getPath();
+   }
+
+   public String getEtlParms() {
+      return getOutputDir() + " " + getOmopDb() + " " + getOutputDir() + "/vizDb/" + getProjectName();
+   }
+
+   public String getVizParms() {
+      return "/vizDb/" + getProjectName();
    }
 
 
